3a39c7ec099e9c56a9a51021d50228f4f00be61e
venkataramanareddy <venkataramanareddy@10.249.34.164>
2019-04-17 22:04:30 +0200

    adding TimeStamp format

---
+++ DateFieldFor/src/main/java/com/example/DateFieldFor/MyUI.java
@@ -0,0 +1,66 @@
+package com.example.DateFieldFor;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Date;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.DateField;
+import com.vaadin.ui.DateTimeField;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+/**
+ * This UI is the application entry point. A UI may either represent a browser
+ * window (or tab) or some part of an HTML page where a Vaadin application is
+ * embedded.
+ * <p>
+ * The UI is initialized using {@link #init(VaadinRequest)}. This method is
+ * intended to be overridden to add component to the user interface and
+ * initialize non-component functionality.
+ */
+@Theme("mytheme")
+public class MyUI extends UI {
+	
+	private Timestamp dat;
+
+	public Timestamp getDat() {
+		return dat;
+	}
+
+	public void setDat(Timestamp timestamp) {
+		this.dat = timestamp;
+	}
+
+	@Override
+	protected void init(VaadinRequest vaadinRequest) {
+		final VerticalLayout layout = new VerticalLayout();
+
+		DateTimeField date = new DateTimeField();
+		date.addContextClickListener(e->{
+			Timestamp timestamp = Timestamp.valueOf(date.getValue());
+			setDat(timestamp);{
+			System.out.println(getDat());}
+		});
+		
+		layout.addComponents(date);
+
+		setContent(layout);
+	}
+
+	@WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
+	@VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
+	public static class MyUIServlet extends VaadinServlet {
+	}
+}
